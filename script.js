3d98e5ac144e21f923e8c295147d2dce7406c652
bruno-andradejc <108308139+bruno-andradejc@users.noreply.github.com>
2022-08-25 19:57:38 -0300

    aT

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log()
+
+function criarParagrafo() {
+    let para = document.createElement('p');
+    para.textContent = 'você clicou no botão!';
+    document.body.appendChild(para);
+}
+
+const botoes = document.querySelectorAll('button');
+
+for(var i = 0; i < botoes.length ; i++) {
+    botoes[i].addEventListener('click', criarParagrafo);
+}
+
